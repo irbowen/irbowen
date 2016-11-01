4d8572966952036cc3550f074d75f7f1785bc621
Isaac Bowen <irbowen@umich.edu>
2016-10-31 20:53:14 -0400

    Setup script for new machines LG

--- .always_install.sh
+++ .always_install.sh
@@ -1,16 +1,23 @@
 
+# Get all of our packages up to date
 sudo apt update
 
 # Install the essentials!
 sudo apt install vim git 
 
-sudo apt install gcc clang
-sudo apt-get install build-essential cmake
-sudo apt-get install python-dev python3-dev
+# Compilers
+sudo apt install gcc clang g++ build-essential cmake
 
-sudo apt-get install python-pip python-dev
+# Python 
+sudo apt install python-dev python3-dev python-pip python3-pip
+
+# Cool tools
 sudo pip install psutil thefuck
 
+# Trash-CLI, so we don't use rm or rm -rf and do somethign stupid
+git clone https://github.com/andreafrancia/trash-cli.git
+cd trash-cli
+sudo python setup.py install
 
 # Install pathogen and Vundle for vim, so that we can use plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
@@ -27,3 +34,5 @@ cd ~/.vim/bundle/YouCompleteMe
 
 # You may still have to run PluginInstall in vim
 echo "You may still need to run ':PluginInstall' within Vim"
+
+vim +PluginInstall +qall
